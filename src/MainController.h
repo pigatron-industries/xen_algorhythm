0e5e1ecd2896acf323eaff937a3191ba0c5e95c7
Robert Ellis <rob@pigatron-industries.co.uk>
2021-12-28 15:51:54 +0000

    Remove unneeded code

--- src/MainController.h
+++ src/MainController.h
@@ -4,8 +4,6 @@
 #include <eurorack.h>
 #include "hwconfig.h"
 #include "Controller.h"
-#include "controllers/euclideanlogic/EuclideanLogicController.h"
-#include "controllers/clockdivider/ClockDividerController.h"
 
 #define CHANNELS 4
 #define OUTPUTS 8
@@ -14,13 +12,6 @@ class MainController {
     public:
         static MainController instance;
 
-        enum Mode {
-            EUCLID_ASYNCHRONOUS,
-            EUCLID_SYNCHRONOUS,
-            EUCLID_DUAL_SEQUENTIAL,
-            CLOCK_DIVIDER
-        };
-
         MainController();
         void init();
         void update();
@@ -28,12 +19,7 @@ class MainController {
         void registerController(Controller& controller);
 
     private:
-        CycleEnum<Mode> mode = CycleEnum<Mode>(Mode::EUCLID_ASYNCHRONOUS, Mode::CLOCK_DIVIDER);
-
         ControllerList<Controller, 2> controllers;
-
-        EuclideanLogicController euclideanLogicController;
-        ClockDividerController clockDividerController;
 
         void controllerInit();
 };
